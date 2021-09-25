8109c13ced59aad5cf97e34ac53b67be5f6bda55
John Stilley <thejollysin@gmail.com>
2021-09-25 11:18:35 -0700

    No failures, but the table isn't rendering

--- src/components/HotSauceTable.vue
+++ src/components/HotSauceTable.vue
@@ -1,14 +1,131 @@
 <template>
-  <div class="hello">
+  <div id="demo">
     <h1>Hello, pepper heads!</h1>
+    <form id="search">
+      Search <input name="query" v-model="searchQuery" />
+    </form>
+    <br />
+    <demo-grid :heroes="gridData" :columns="gridColumns" :filter-key="searchQuery">
+    </demo-grid>
   </div>
+</template>
+
+<template>
+<div id="grid-template">
+  <table>
+    <thead>
+      <tr>
+        <th v-for="key in columns" @click="sortBy(key)" :class="{ active: sortKey == key }">
+          {{ key | capitalize }}
+          <template v-if="key != 'food_pairings' && key != 'flavor'">
+            <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+            </span>
+          <template v-else>
+          </template>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="entry in filteredHeroes">
+        <td v-for="key in columns">
+          {{entry[key]}}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HotSauceTable',
+  template: "#grid-template",
   props: {
-    msg: String
+    heroes: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function() {
+    var sortOrders = {};
+    this.columns.forEach(function(key) {
+      sortOrders[key] = 1;
+    });
+    return {
+      sortKey: "",
+      sortOrders: sortOrders
+    };
+  },
+  computed: {
+    filteredHeroes: function() {
+      var sortKey = this.sortKey;
+      var filterKey = this.filterKey && this.filterKey.toLowerCase();
+      var order = this.sortOrders[sortKey] || 1;
+      var heroes = this.heroes;
+      if (filterKey) {
+        heroes = heroes.filter(function(row) {
+          return Object.keys(row).some(function(key) {
+            return (
+              String(row[key])
+              .toLowerCase()
+              .indexOf(filterKey) > -1
+            );
+          });
+        });
+      }
+      if (sortKey) {
+        heroes = heroes.slice().sort(function(a, b) {
+          a = a[sortKey];
+          b = b[sortKey];
+          return (a === b ? 0 : a > b ? 1 : -1) * order;
+        });
+      }
+      return heroes;
+    }
+  },
+  filters: {
+    capitalize: function(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1).replaceAll("_", " ");
+    }
+  },
+  methods: {
+    sortBy: function(key) {
+      this.sortKey = key;
+      this.sortOrders[key] = this.sortOrders[key] * -1;
+    }
+  }
+}
+</script>
+
+<script>
+export default {
+  el: "#demo",
+  data: {
+    searchQuery: "",
+    gridColumns: ["sauce", "rating", "SHU", "food_pairings"],
+    gridData: [{
+        sauce: "Sriracha",
+        rating: 80,
+        SHU: 2200,
+        food_pairings: "Thai, Eggs"
+      },
+      {
+        sauce: "Tobasco",
+        rating: 20,
+        SHU: 3750,
+        food_pairings: "Eggs"
+      },
+      {
+        sauce: "Peri Peri - XX Hot",
+        rating: 90,
+        SHU: 35000,
+        food_pairings: "Eggs, grilled vegetables"
+      },
+      {
+        sauce: "Iguana Gold",
+        rating: 95,
+        SHU: 6000,
+        food_pairings: "Pretzels, Sausages"
+      }
+    ]
   }
 }
 </script>
