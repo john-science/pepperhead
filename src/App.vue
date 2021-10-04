fc5fa94a28b5152436bf599e28cad77ec6ddc8d7
John Stilley <thejollysin@gmail.com>
2021-10-03 17:34:26 -0700

    Turning the reviews table into a more modular component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <img alt="PepperHead logo" src="./assets/hot_sauces.png">
-    <HotSauceTable msg="Welcome to PepperHead"/>
+    <h3>Hot Sauce Reviews</h3>
+    <h4>by... Some Pepper Head</h4>
+
+    <HotSauceTable msg="Hot Sauce Reviews"/>
+
+    <br />
+    <p>The reviews above are obviously based on personal taste. I obviously like spicy sauces, but I also like to use <strong>a lot</strong> of sauce, so I tend to dislike vinegar-heavy sauces. Please
+    don't send me hate mail if you disagree with these ratings. Perhaps some day I will build a community hot sauce review site, where you can make your own voice heard.</p>
   </div>
 </template>
 
--- src/components/HotSauceTable.vue
+++ src/components/HotSauceTable.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <h3>Hot Sauce Reviews</h3>
-    <h4>by... some pepper head</h4>
-
     <form id="search">
       Search <input name="query" class="inputClass" v-model="searchQuery" />
     </form>
@@ -23,10 +20,6 @@
         </tr>
       </tbody>
     </table>
-
-    <br />
-    <p>The reviews above are obviously based on personal taste. I obviously like spicy sauces, but I also like to use <strong>a lot</strong> of sauce, so I tend to dislike vinegar-heavy sauces. Please
-    don't send me hate mail if you disagree with these ratings. Perhaps some day I will build a community hot sauce review site, where you can make your own voice heard.</p>
   </div>
 </template>
 
