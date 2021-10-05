f9c60ea242df05897d37dfa613c8219c2c1dec23
John Stilley <thejollysin@gmail.com>
2021-10-04 19:23:30 -0700

    Moving NavBar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div id="app">
-    <!-- TODO: Move nav bar to its own component -->
-    <div id="nav" class="header">
-      <table class="hidden-table">
-        <tbody>
-          <tr class="hidden-table">
-            <td>
-              <p class="header-logo"><a href="#">PepperHead</a></p>
-            </td>
-            <td>
-              <nav class="nav-global">
-                <ul>
-                  <li><a href="#">Reviews</a></li>
-                  <li><a href="#">Wanted</a></li>
-                </ul>
-              </nav>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <NavBar />
+
     <h2>Hot Sauce Reviews</h2>
     <p>by Some Pepper Head</p>
     <br />
 
-    <HotSauceTable msg="Hot Sauce Reviews"/>
+    <HotSauceTable />
 
     <br />
     <p>The above are just my opinions, obviously. For instance, I'm not a big fan of Louisiana hot sauces. Also, I am only reviewing commercially available hot sauces.</p>
@@ -48,11 +30,13 @@
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
 import HotSauceTable from './components/HotSauceTable.vue'
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HotSauceTable
   }
 }
@@ -87,64 +71,5 @@ h3,h4 {
 a:active, a:hover, a:link, a:visited {
   color:green;
   text-decoration:none;
-}
-
-.header {
-  display:block;
-  padding-left:1em;
-  padding-right:1em;
-}
-
-.header-logo {
-  font-size:24px;
-  font-weight: bolder;
-  font-family: monospace, monospace;
-  margin:0px;
-  padding-top:.75em;
-  padding-bottom:.75em;
-}
-
-.header-logo a:active, .header-logo a:hover, .header-logo a:link, .header-logo a:visited {
-  color:#9f2a00;
-  text-decoration:none;
-  font-weight: bolder;
-}
-
-.nav-global {
-  font-weight: bolder;
-  margin-top:0;
-  text-align:right;
-  width:15em;
-  float:right;
-}
-
-.nav-global a:active, .nav-global a:hover, .nav-global a:link, .nav-global a:visited {
-  color:#017924;
-  text-decoration:none;
-  font-weight: bolder;
-}
-
-.nav-global ul {
-  list-style:none;
-  list-style-image:none;
-  margin:0;
-  padding:0;
-}
-
-.nav-global li {
-  display:inline-block;
-  line-height:normal;
-  letter-spacing:normal;
-  margin-right:16px;
-  word-spacing:normal;
-  vertical-align:middle;
-}
-
-.hidden-table {
-  border:0;
-  margin:0;
-  padding:0;
-  border-spacing:0;
-  width:90%;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="nav" class="header">
+    <table class="hidden-table">
+      <tbody>
+        <tr class="hidden-table">
+          <td>
+            <p class="header-logo"><a href="#">PepperHead</a></p>
+          </td>
+          <td>
+            <nav class="nav-global">
+              <ul>
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#">Wanted</a></li>
+              </ul>
+            </nav>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+.header {
+  display:block;
+  padding-left:1em;
+  padding-right:1em;
+}
+
+.header-logo {
+  font-size:24px;
+  font-weight: bolder;
+  font-family: monospace, monospace;
+  margin:0px;
+  padding-top:.75em;
+  padding-bottom:.75em;
+}
+
+.header-logo a:active, .header-logo a:hover, .header-logo a:link, .header-logo a:visited {
+  color:#9f2a00;
+  text-decoration:none;
+  font-weight: bolder;
+}
+
+.nav-global {
+  font-weight: bolder;
+  margin-top:0;
+  text-align:right;
+  width:15em;
+  float:right;
+}
+
+.nav-global a:active, .nav-global a:hover, .nav-global a:link, .nav-global a:visited {
+  color:#017924;
+  text-decoration:none;
+  font-weight: bolder;
+}
+
+.nav-global ul {
+  list-style:none;
+  list-style-image:none;
+  margin:0;
+  padding:0;
+}
+
+.nav-global li {
+  display:inline-block;
+  line-height:normal;
+  letter-spacing:normal;
+  margin-right:16px;
+  word-spacing:normal;
+  vertical-align:middle;
+}
+
+.hidden-table {
+  border:0;
+  margin:0;
+  padding:0;
+  border-spacing:0;
+  width:90%;
+}
+</style>
