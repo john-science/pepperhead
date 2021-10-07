8bc85e8e19fcc0f5f11af3f0a2e7da85993209bc
John Stilley <thejollysin@gmail.com>
2021-10-07 07:22:40 -0700

    Cleaning up desirable sauces page

--- src/pages/Wanted.vue
+++ src/pages/Wanted.vue
@@ -4,16 +4,21 @@
 
     <h2>Hot Sauces I Want to Try</h2>
     <ul class="big-list">
-      <li>Bonache - Piri Piri sauce - Washington state</li>
-      <li>Mike's Fine Brines - Chipolte - Washington state</li>
-      <li>Hot Girl Hot Saucei - Jamacian style - Washington state</li>
-      <li>Aardvark Sauce - Oregon</li>
-      <li><a href="https://www.seattlemag.com/restaurants/washington%E2%80%99s-hot-sauce-game-fire">Check out this list of locals</a></li>
-      <li><a href="https://www.amazon.com/dp/B004MXR9AU/">Pain is Good - Habanero Sauce</a></li>
-      <li><a href="https://www.amazon.com/dp/B06XX4R3GX/">Ghost Pepper Hot Sauce by Yellowbird</a></li>
-      <li><a href="https://www.amazon.com/dp/B00BADXRRO">Rogue Moruga Blood Orange Scorpion Pepper Sauce</a></li>
-      <li><a href="https://www.amazon.com/dp/B06XF8SRHY/">QUEEN MAJESTY HOT SAUCE Hot Sauce Red Habanero Black Coffee</a></li>
-      <li><a href="https://www.amazon.com/dp/B01KB7XZ62">Cheeba Gold Barbados Style Scotch Bonnet Pepper Sauce</a></li>
+      <li><a href="https://highriversauces.com/" target="_blank">High River</a> - <a href="https://www.amazon.com/dp/B01KB7XZ62" target="_blank">Cheeba Gold Barbados Style Scotch Bonnet Pepper Sauce</a></li>
+      <li><a href="https://highriversauces.com/" target="_blank">High River</a> - <a href="https://www.amazon.com/dp/B00BADXRRO" target="_blank">Rogue Moruga Blood Orange Scorpion Pepper Sauce</a></li>
+      <li><a href="https://www.amazon.com/dp/B004MXR9AU/" target="_blank">Pain is Good - Habanero Sauce</a></li>
+      <li><a href="https://www.amazon.com/dp/B06XF8SRHY/" target="_blank">Queen Majesty - Red Habanero Black Coffee</a></li>
+      <li><a href="https://www.yellowbirdfoods.com/" target="_blank">Yellowbird</a> - <a href="https://www.amazon.com/dp/B06XX4R3GX/" target="_blank">Ghost Pepper Hot Sauce</a></li>
+    </ul>
+
+    <h4>Sauces Local to Me</h4>
+    <ul class="big-list">
+      <li><a href="https://bonachesauce.com/" target="_blank">Bonache</a> - Piri Piri sauce</li>
+      <li>Hot Girl Hot Sauce - Jamacian style</li>
+      <li><a href="http://www.mikesfinebrines.com/" target="_blank">Mike's Fine Brines</a> - Callahan's Chipolte</li>
+      <li><a href="https://secretaardvark.com/" target="_blank">Secret Aardvark</a></li>
+      <li><a href="https://www.silagysauce.com/shop" target="_blank">Silagy Sauces</a> - <a href="https://www.silagysauce.com/shop/roasted-ghost" target="_blank">Roasted Ghost Sauce</a></li>
+      <li><a href="https://www.silagysauce.com/shop" target="_blank">Silagy Sauces</a> - <a href="https://www.silagysauce.com/shop/smokin-habanero-sauce" target="_blank">Smokin' Habanero Sauce</a></li>
     </ul>
   </div>
 </template>
@@ -33,7 +38,7 @@
 <style scoped>
 .big-list {
   text-align: left;
-  margin-left: 25%;
+  margin-left: 20%;
 }
 </style>
 
