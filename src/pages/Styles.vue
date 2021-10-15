72651e9b81247138389ea3e9b92721193e3f9c93
John Stilley <thejollysin@gmail.com>
2021-10-15 15:29:44 -0700

    Showing some examples of a particular sauce style

--- src/pages/Styles.vue
+++ src/pages/Styles.vue
@@ -3,12 +3,18 @@
     <NavBar />
 
     <h2>Hot Sauce Styles</h2>
+    <p>At some point, the variety of hot sauces just sky-rocketed. But before the craft hot sauce movement, there were distinct styles of spicy sauces around the world. We attempt to identify the most popular here.</p>
+   <br/>
+
     <div class="center-it">
-      <ul class="big-list">
-        <li v-for="item in data" :key="item">
-          {{ item.style }} - {{ item.desc }}
-        </li>
-      </ul>
+      <template>
+        <div v-for="item in data" :key="item">
+          <h3>{{ item.style }}</h3>
+          <h4 v-if="item.examples.length > 0">{{ item.examples }}</h4>
+          <p>{{ item.desc }}</p>
+          <br/>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -19,6 +25,24 @@
   import styleData from "../assets/styles_of_sauces.json";
   import sauceData from "../assets/hot_sauce_reviews.json";
 
+  // pre-parsing styles from all reviewed hot sauces
+  let styleToSauce = {};
+  for (let item in styleData) {
+    styleToSauce[styleData[item].style] = [];
+  }
+
+  for (let item in sauceData) {
+    styleToSauce[sauceData[item].style].push(sauceData[item].sauce);
+  }
+
+  for (let item in styleData) {
+    styleData[item].examples = [];
+    let exs = styleToSauce[styleData[item].style];
+    if (exs && exs.length > 0) {
+      styleData[item].examples = exs.slice(0, 3);
+    }
+  }
+
   export default {
     name: 'Styles',
     components: {
@@ -26,7 +50,6 @@
     },
     data: function() {
       this.data = styleData;
-      this.sauces = sauceData;  // TODO: We probably don't need this, just pre-process out the sauces of interest.
     },
   };
 </script>
